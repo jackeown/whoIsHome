bff21ae2da7cf3d6156d3d047735849d44eb95da
Jack McKeown <jackamckeown@gmail.com>
2022-02-10 12:23:44 -0500

    latest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='whoishome',
-    version='0.0.7',
+    version='0.0.8',
     url="https://github.com/jackeown/whoishome",
     description = ("A simple python CLI dashboard which uses nmap to help you keep track of who's on your network (made beautiful with the python library 'rich')."),
     long_description=open('README.md').read(),
