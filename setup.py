2d09943d550eee04b6b123b2a42d719d0c533b63
Jack McKeown <jackamckeown@gmail.com>
2022-02-09 23:51:08 -0500

    added stats streaming stuff

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='whoishome',
-    version='0.0.6',
+    version='0.0.7',
     url="https://github.com/jackeown/whoishome",
     description = ("A simple python CLI dashboard which uses nmap to help you keep track of who's on your network (made beautiful with the python library 'rich')."),
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     scripts=['whoishome/whoishome'],
     install_requires=[
-        'rich',
+        'rich', 'matplotlib'
     ],
 )
